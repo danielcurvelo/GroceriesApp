56799b016d9b94f5149c014ea60e1e5fd60bf710
Daniel Curvelo <daniel.curvelo@hotmail.com>
2015-09-15 15:59:47 -0600

    commiting grocery controller

--- GroceriesApp/GroceriesApp/GroceryController.swift
+++ GroceriesApp/GroceriesApp/GroceryController.swift
@@ -7,7 +7,37 @@
 //
 
 import UIKit
+import CoreData
 
 class GroceryController: NSObject {
+    
+    let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+    func createItemInCategory(category:Category, name:String, shelfLife: NSNumber, tags: NSOrderedSet)
+    {
+        let item = NSEntityDescription.insertNewObjectForEntityForName("Item", inManagedObjectContext:appDelegate.managedObjectContext!) as! Item
+        item.name = name
+        item.category = category
+        item.shelfLife = shelfLife
+        item.tags = tags
+        save()
+    }
+
+    var categories:[Category]
+    {
+        get{
+            let fetchRequest = NSFetchRequest(entityName:"Category")
+            return appDelegate.managedObjectContext?.executeFetchRequest(fetchRequest, error:nil) as! [Category]
+        }
+    }
+    
+    func save()
+    {
+        appDelegate.managedObjectContext?.save(nil)
+    }
+    
+    
+    
+
    
 }
