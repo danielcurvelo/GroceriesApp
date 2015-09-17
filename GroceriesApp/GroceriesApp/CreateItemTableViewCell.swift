9be1c1d5a2826b1bc0e3ab7b912165099e4ea5ec
Doug <dougrgoodwin@gmail.com>
2015-09-17 15:13:36 -0600

    Storyboard / Navigation
    Yeah.  What it sounds like, that's what it is.

---
+++ GroceriesApp/GroceriesApp/CreateItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CreateItemTableViewCell.swift
+//  GroceriesApp
+//
+//  Created by Douglas Goodwin on 9/17/15.
+//  Copyright © 2015 DanielCurvelo. All rights reserved.
+//
+
+import UIKit
+
+class CreateItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
