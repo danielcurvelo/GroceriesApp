6d37abf4799f617e8454905ce8623a5ea3249712
Robert Shepperd <shepperdr@hotmail.com>
2015-09-25 11:40:21 -0600

    playground and expiration added

---
+++ GroceriesApp/GroceriesApp/ExpirationController.swift
@@ -0,0 +1,13 @@
+//
+//  ExpirationController.swift
+//  GroceriesApp
+//
+//  Created by Robert Shepperd on 9/25/15.
+//  Copyright © 2015 DanielCurvelo. All rights reserved.
+//
+
+import UIKit
+
+class ExpirationController: NSObject {
+
+}
---
+++ GroceriesApp/GroceriesApp/Practice .playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var purchaseDate = NSDate()
+var shelfLife = 7
+
+
+
