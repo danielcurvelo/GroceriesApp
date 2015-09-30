4c678258c7349b5f5dc33c51d7497505363b8c5e
Doug <dougrgoodwin@gmail.com>
2015-09-29 18:49:36 -0600

    New Cells

---
+++ GroceriesApp/GroceriesApp/ExpiredTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExpiredTableViewCell.swift
+//  GroceriesApp
+//
+//  Created by Douglas Goodwin on 9/29/15.
+//  Copyright © 2015 DanielCurvelo. All rights reserved.
+//
+
+import UIKit
+
+class ExpiredTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
